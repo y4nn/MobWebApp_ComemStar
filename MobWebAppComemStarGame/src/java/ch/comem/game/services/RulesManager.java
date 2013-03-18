863ec268a673bcb190a0cbc55c1a2112dcc53b04
Sandra <sandra.gindroz@heig-vd.ch>
2013-03-18 09:43:37 +0100

    test sandra

---
+++ MobWebAppComemStarGame/src/java/ch/comem/game/services/RulesManager.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.comem.game.services;
+
+import ch.comem.game.model.Rule;
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author admin
+ */
+@Stateless
+@WebService
+public class RulesManager implements RulesManagerLocal {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public Rule createRules(String eventType, String nbPts) {
+        Rule rule = new Rule();
+        rule.setEventType(eventType);
+        rule.setNbPts(nbPts);
+        em.persist(rule);
+        em.flush();
+        return rule;
+    }
+
+
+    
+    
+}
---
+++ MobWebAppComemStarGame/src/java/ch/comem/game/services/RulesManagerLocal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.comem.game.services;
+
+import ch.comem.game.model.Rule;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author admin
+ */
+@Local
+public interface RulesManagerLocal {
+
+
+
+    Rule createRules(String eventType, String nbPts);
+    
+}
