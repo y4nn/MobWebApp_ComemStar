0578a057097c8dbb9c1ace563912ba8336a07550
Zikrija Saric <zikrija.saric@gmail.com>
2013-03-28 08:34:12 +0100

    Structure

--- MobWebAppComemStarAppli/src/java/ch/comem/appli/tests/TestsManager.java
+++ MobWebAppComemStarAppli/src/java/ch/comem/appli/tests/TestsManager.java
@@ -101,20 +101,60 @@ public class TestsManager implements TestsManagerLocal {
         Serie serieNewsPerti    = this.seriesManager.createSerie("Pertinance des articles", coursNEWS);
         
         
-        Question q1 = this.questionsManager.createQuestion("JEE: Combien font 2x2 ?", serieIntroJEE);
+        Question q1 = this.questionsManager.createQuestion("Combien font 2x2 ?", serieIntroJEE);
         this.answersManager.createAnswer("42", Boolean.FALSE, q1);
         this.answersManager.createAnswer("4", Boolean.TRUE, q1);
-        this.answersManager.createAnswer("Demande à Chuck Norris", Boolean.FALSE, q1);
+        this.answersManager.createAnswer("21", Boolean.FALSE, q1);
         
-        Question q2 = this.questionsManager.createQuestion("JEE: Combien font 3+10 ?", serieIntroJEE);
+        Question q2 = this.questionsManager.createQuestion("Combien font 3+10 ?", serieIntroJEE);
         this.answersManager.createAnswer("13", Boolean.TRUE, q2);
         this.answersManager.createAnswer("4", Boolean.FALSE, q2);
-        this.answersManager.createAnswer("Demande à Chuck Norris", Boolean.FALSE, q2);
+        this.answersManager.createAnswer("17", Boolean.FALSE, q2);
         
-        Question q3 = this.questionsManager.createQuestion("REST: Combien font 6x7 ?", serieREST);
-        this.answersManager.createAnswer("42", Boolean.TRUE, q3);
-        this.answersManager.createAnswer("4", Boolean.FALSE, q3);
-        this.answersManager.createAnswer("Demande à Chuck Norris", Boolean.FALSE, q3);
+        Question q3 = this.questionsManager.createQuestion("Combien font 3x3 ?", serieIntroJEE);
+        this.answersManager.createAnswer("27", Boolean.FALSE, q3);
+        this.answersManager.createAnswer("9", Boolean.TRUE, q3);
+        this.answersManager.createAnswer("6", Boolean.FALSE, q3);
+        
+        Question q4 = this.questionsManager.createQuestion("Combien font 3x0 ?", serieIntroJEE);
+        this.answersManager.createAnswer("0", Boolean.TRUE, q4);
+        this.answersManager.createAnswer("4", Boolean.FALSE, q4);
+        this.answersManager.createAnswer("3", Boolean.FALSE, q4);
+        
+        Question q5 = this.questionsManager.createQuestion("Combien font 8/2 ?", serieIntroJEE);
+        this.answersManager.createAnswer("6", Boolean.FALSE, q5);
+        this.answersManager.createAnswer("2", Boolean.FALSE, q5);
+        this.answersManager.createAnswer("4", Boolean.TRUE, q5);
+        
+        /*Question q6 = this.questionsManager.createQuestion("Combien font 13*2 ?", serieIntroJEE);
+        this.answersManager.createAnswer("13", Boolean.FALSE, q6);
+        this.answersManager.createAnswer("26", Boolean.TRUE, q6);
+        this.answersManager.createAnswer("27", Boolean.FALSE, q6);
+        
+        Question q7 = this.questionsManager.createQuestion("Combien font 8x9 ?", serieIntroJEE);
+        this.answersManager.createAnswer("98", Boolean.FALSE, q7);
+        this.answersManager.createAnswer("17", Boolean.FALSE, q7);
+        this.answersManager.createAnswer("81", Boolean.TRUE, q7);
+        
+        Question q8 = this.questionsManager.createQuestion("Combien font 8x8 ?", serieIntroJEE);
+        this.answersManager.createAnswer("64", Boolean.TRUE, q8);
+        this.answersManager.createAnswer("16", Boolean.FALSE, q8);
+        this.answersManager.createAnswer("88", Boolean.FALSE, q8);
+        
+        Question q9 = this.questionsManager.createQuestion("Combien font 9+1 ?", serieIntroJEE);
+        this.answersManager.createAnswer("10", Boolean.TRUE, q9);
+        this.answersManager.createAnswer("4", Boolean.FALSE, q9);
+        this.answersManager.createAnswer("11", Boolean.FALSE, q9);
+        
+        Question q10 = this.questionsManager.createQuestion("Combien font 7-4 ?", serieIntroJEE);
+        this.answersManager.createAnswer("10", Boolean.FALSE, q10);
+        this.answersManager.createAnswer("3", Boolean.TRUE, q10);
+        this.answersManager.createAnswer("14", Boolean.FALSE, q10);*/
+        
+        Question q0 = this.questionsManager.createQuestion("REST: Combien font 6x7 ?", serieREST);
+        this.answersManager.createAnswer("42", Boolean.TRUE, q0);
+        this.answersManager.createAnswer("4", Boolean.FALSE, q0);
+        this.answersManager.createAnswer("Demande à Chuck Norris", Boolean.FALSE, q0);
         
         
     }
